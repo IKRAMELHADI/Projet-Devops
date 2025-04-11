f6a475f1ec55c157917f4d35fb3a5b8875c2d853
IKRAMELHADI <152861932+IKRAMELHADI@users.noreply.github.com>
2025-04-11 13:58:27 +0200

    Ajout du menu

--- front-end/src/nav.js
+++ front-end/src/nav.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import './App.css'
+import { Link
+ } from 'react-router-dom';
 
 const Nav=()=>{
     return (
         <div>
-            Header file
+            <ul className="nav-ul">
+                <li><Link to="/">Products</Link></li>
+                <li><Link to="/add">Add products</Link></li>
+                <li><Link to="/update">Update products</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+            </ul>
         </div>
     )
 
